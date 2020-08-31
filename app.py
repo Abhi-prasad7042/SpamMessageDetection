55bce605227e71544189b25dc37d08052a8cbfe6
Abhi Prasad <AbhiPrasad7042@gmail.com>
2020-08-31 20:14:28 +0530

    End to End Deployment

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template, jsonify, request
+import numpy as np
+import string
+from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer
+import pickle
+
+app = Flask(__name__)
+ps = PorterStemmer()
+
+def text_process(mess):
+    nopunc = [char for char in mess if char not in string.punctuation]
+    nopunc = ''.join(nopunc)
+    clean_mess = [word for word in nopunc.split() if word.lower() not in stopwords.words('english')]
+    word = [ps.stem(word) for word in clean_mess]
+    word = " ".join(word)
+    return word
+
+model = pickle.load(open("spam-message-detection.pkl", "rb"))
+
+@app.route('/')
+def index():
+    return render_template("home.html")
+
+
+@app.route('/predict', methods=["POST"])
+def predict():
+    mess = request.form.values()
+    output = model.predict([mess])[0]
+    return render_template("predict.html", output = output)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
